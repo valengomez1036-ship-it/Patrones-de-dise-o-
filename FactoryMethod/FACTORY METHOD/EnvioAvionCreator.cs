65a0bf2ee0ad272f80097313b0d11673a57cb0ed
valengomez1036-ship-it <valen.gomez1036@gmail.com>
2026-04-24 11:10:15 -0500

    Add files via upload

---
+++ FactoryMethod/FACTORY METHOD/EnvioAvionCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodN
+{
+    public class EnvioAvionCreator : EnvioCreator
+    {
+        protected override IEnvio CrearEnvio()
+        {
+            return new EnvioAvion();
+        }
+    }
+}
---
+++ FactoryMethod/FACTORY METHOD/EnvioCamionCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodN
+{
+    public class EnvioCamionCreator : EnvioCreator
+    {
+        protected override IEnvio CrearEnvio()
+        {
+            return new EnvioCamion();
+        }
+    }
+}
